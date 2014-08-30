2773ec096bf32d69c8f905a9f18c3a736022bdcf
Stefano Verna <s.verna@cantierecreativo.net>
2014-08-30 21:42:22 +0200

    Remove hardcoded namespace

--- app/controllers/concerns/rest_concern.rb
+++ app/controllers/concerns/rest_concern.rb
@@ -11,35 +11,35 @@ module RestConcern
   def index
     @query = query_class.new(params)
     @collection = @query.scope
-    respond_with :admin, @collection
+    respond_with @collection
   end
 
   def new
     @resource = resource_class.new
-    respond_with :admin, @resource
+    respond_with @resource
   end
 
   def create
     @resource = resource_class.new(permitted_params)
     @resource.save
-    respond_with :admin, @resource
+    respond_with @resource, location: collection_url
   end
 
   def edit
     @resource = resource_class.find(params[:id])
-    respond_with :admin, @resource
+    respond_with @resource
   end
 
   def update
     @resource = resource_class.find(params[:id])
     @resource.update_attributes(permitted_params)
-    respond_with :admin, @resource
+    respond_with @resource, location: collection_url
   end
 
   def destroy
     @resource = resource_class.find(params[:id])
     @resource.destroy
-    respond_with :admin, @resource
+    respond_with @resource, location: collection_url
   end
 
   private
